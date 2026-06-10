fa3087721ed23811868acb4cefe7bbf77f7188e6
Faiz Muhammad Ridho <64389142+crash4tx@users.noreply.github.com>
2026-06-10 08:58:00 +0700

    update

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS & JSON parsing
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Serving static frontend files
 app.use(express.static(path.join(__dirname)));
